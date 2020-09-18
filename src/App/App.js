c6a16b37a748dc77b8ec9633408b51dcad168fd3
osund <osundal83@gmail.com>
2020-09-18 13:59:48 +0200

    add api key for cryptocompare

--- src/App/App.js
+++ src/App/App.js
@@ -4,15 +4,20 @@ import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 import {AppProvider} from "./AppProvider";
 import Settings from "../Settings/index.js"
+import DashBoard from "../Dashboard/index.js"
 import Content from "../Shared/Content";
+import {setApiKey} from "cryptocompare";
 
 function App() {
+    setApiKey('916d275830a4bf79fa4156f45cb7d76e0e4a7539a4a082362a24c57915e38b26');
+
     return (
         <AppLayout>
             <AppProvider>
                 <AppBar />
                 <Content>
                     <Settings />
+                    <DashBoard />
                 </Content>
             </AppProvider>
         </AppLayout>
